89ee74fd836ff4470f366163bd95c42dcf95c39a
SuJinTuan <sujin810969050@qq.com>
2020-11-05 23:43:45 +0800

    add all file

--- src/views/detail/childComponents/DetailSwiper.vue
+++ src/views/detail/childComponents/DetailSwiper.vue
@@ -1,11 +1,9 @@
 <template>
- 
   <swiper class="detail-swiper">
     <swiper-item v-for="item in topImages">
       <img :src="item" alt="" />
     </swiper-item>
   </swiper>
-
 </template>
 
 <script>
